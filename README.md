db6db69e44b3fdb01d42500cbc55b5547b9bb733
shibbo <xarf98@yahoo.com>
2017-09-28 22:59:53 -0400

    add files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BMTInject
 A tool for injecting BMT files into BMD files. Part of the J3D engine.
+
+# How To Use
+Use is simple. You need the BMD to be injected into, and the BMT to inject. Syntax is as follows:
+
+```
+bmtinject.py in.bmd in.bmt out.bmd
+```
+
+Happy hax.
---
+++ bmtinject.py
@@ -0,0 +1,77 @@
+import struct, sys
+
+def injectBMT(in_bmd, in_bmt, out_bmd):
+    with open(in_bmd, "rb") as f:
+        bmdData = f.read()
+    with open(in_bmt, "rb") as f:
+        bmtData = f.read()
+
+    # file verification
+    # if it doesnt exist it would have thrown a trackback by now
+    bmd_magic, = struct.unpack_from('>I', bmdData, 0)
+    bmt_magic, = struct.unpack_from('>I', bmtData, 0)
+
+    if bmd_magic != 0x4A334432:
+        print("Not a valid BMD file.")
+        return
+    else:
+        print("File is a valid BMD.")
+
+    if bmt_magic != 0x4A334432:
+        print("Not a valid BMT file.")
+        return
+    else:
+        print("File is a valid BMT file.")
+
+    print("Attempting to get BMD data...")
+    print("Trying to find TEX1 section...")
+    # we start at INF1
+    offset = 0x20
+    while(True):
+        tempStr, = struct.unpack_from('4s', bmdData, offset)
+        if tempStr == b'INF1':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'VTX1':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'EVP1':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'DRW1':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'JNT1':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'SHP1':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'MAT3':
+            tempInt, = struct.unpack_from('>I', bmdData, offset+4)
+            offset+=tempInt
+        elif tempStr == b'TEX1':
+            print("Found TEX1!")
+            bmdFullData = bytearray(bmdData[:offset])
+            break
+        else:
+            print("Error! Found string " + str(tempStr))
+            break
+
+    print("Attempting to get BMT data...")
+    bmtPos = 0x20
+    bmtTEX1 = bytearray(bmtData[bmtPos:])
+    print("Success!")
+
+    print("Now writing to file...")
+    with open(out_bmd, "wb") as f:
+        f.write(bmdFullData)
+        f.write(bmtTEX1)
+
+    print("Done!")
+
+if len(sys.argv) < 4:
+    print("BMT Inject v0.1 by shibboleet")
+    print("Syntax: bmtinject.py in.bmd in.bmt out.bmd")
+else:
+    injectBMT(sys.argv[1], sys.argv[2], sys.argv[3])
